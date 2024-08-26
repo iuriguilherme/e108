9969bbbea0e09b338a003aff1cf000e5267685a7
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2024-08-26 15:25:29 -0300

    mariadb

--- scripts/atualizatodomundo.py
+++ scripts/atualizatodomundo.py
@@ -4,10 +4,16 @@ import logging
 logging.basicConfig(level = 'INFO')
 logger: logging.Logger = logging.getLogger(__name__)
 
-
 import requests
+import sys
 
 api: str = "https://habborigins.com.br/api/v2"
+
+dias: int = 2
+try:
+    dias = sys.argv[1]
+except:
+    pass
 
 try:
     with open('docs/users.txt', 'r') as f:
@@ -15,7 +21,7 @@ try:
     for user in users:
         logger.info(f"Atualizando {user}")
         requests.get("/".join([api, "atualizar",
-            "partidas", user]))
+            "partidas", f"{user}?last_day={dias}"]))
         requests.get("/".join([api, "atualizar",
             "placar", "um", user]))
 except Exception as e:
